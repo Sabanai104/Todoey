d8884b5f3e4b8a80f0864c22ba6ba700dc48fc07
Gabriel Sabanai <gabrielsabanaitrindade@gmail.com>
2024-11-27 19:41:24 -0300

    Initial Commit

---
+++ Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by Gabriel Sabanai on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
